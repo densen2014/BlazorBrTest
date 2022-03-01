218d49aacaef5d0f379b8304ff09a6aa7aad2f94
Alex Chow <zhouchuanglin@gmail.com>
2022-03-01 16:25:01 +0100

    PWA不能用import { BrotliDecode } from './decode.min.js'; 直接会导致pwa离线功能失效 , 就走nginx自己的br就够  hash校验可以强制关掉或者自己再算一遍.  .map(asset => new Request(asset.url, {  cache: 'no-cache' }));  空wasm [非pwa] 工程,用上面的js. 的确纯走br了
    PWA不能用import { BrotliDecode } from './decode.min.js'; 直接会导致pwa离线功能失效 , 就走nginx自己的br就够
    
    hash校验可以强制关掉或者自己再算一遍.  .map(asset => new Request(asset.url, {  cache: 'no-cache' }));
    
    空wasm [非pwa] 工程,用上面的js. 的确纯走br了

---
+++ BlazorWasmPWAHosting/Server/Program.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.ResponseCompression;
+using System.IO.Compression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddResponseCompression(options =>
+{
+    options.Providers.Add<BrotliCompressionProvider>();
+    options.Providers.Add<GzipCompressionProvider>();
+    options.MimeTypes =
+    ResponseCompressionDefaults.MimeTypes.Concat(
+                    new[] { 
+                        "image/svg+xml", 
+                        "application/octet-stream",
+                        "application/json",
+                    });
+});
+builder.Services.Configure<BrotliCompressionProviderOptions>(options =>
+{
+    options.Level = (CompressionLevel)4;
+});
+builder.Services.Configure<GzipCompressionProviderOptions>(config =>
+{
+    config.Level = (CompressionLevel)4;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseResponseCompression();
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
